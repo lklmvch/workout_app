e6832097fc116ab6a109cd2aac7cd3a3ba6251c9
lklmvch <aleska.klimovich@gmail.com>
2024-07-25 21:42:39 +0300

    Added new files

---
+++ workout_app/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def plans(request):
+    return HttpResponse('<ul><li>First plan</li><li>Second plan</li><li>Third plan</li></ul>')
